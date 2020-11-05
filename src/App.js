e7ad99a3c6e836b34486760cc772e458e60c3c31
Marcos Silva <marcos.silva79@gmail.com>
2020-11-05 14:45:01 -0500

    feat: add uSeReducer case

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
+import React, { useContext, useReducer } from 'react';
+import { UserContext } from './index';
 import './App.css';
 
+const initialState = {
+  count: 0
+}
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "increment":
+      return {
+        count: state.count + 1
+      };
+    case "decrement":
+      return {
+        count: state.count - 1
+      };
+    case "reset":
+      return initialState;
+
+    default:
+      return initialState
+  }
+}
+
 function App() {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  const value = useContext(UserContext)
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <div>
+        Count: {state.count}
+        <button onClick={() => dispatch({ type: "increment" })}>Increment</button>
+        <button onClick={() => dispatch({ type: "reset" })}>Reset</button>
+        <button onClick={() => dispatch({ type: "decrement" })}>Decrement</button>
+      </div>
+
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+export const UserContext = React.createContext();
+
+const username = "Marcos";
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <UserContext.Provider value={username}>
+      <App />
+    </UserContext.Provider >
+
   </React.StrictMode>,
   document.getElementById('root')
 );
